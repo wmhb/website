faabaa9bd08c1715769348f3e8ced92d1f45c33f
Florenz Heldermann <hi@flore.nz>
2018-02-09 13:41:43 +0100

    Default: change to match other pages

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,13 +1,16 @@
 <?php snippet('header') ?>
 
-  <main class="main g--col" role="main">
+  <main class="main g g--col" role="main">
 
-    <section class="gi-full">
-        <article class="pane g">
-            <h3 class="gi gi--full gi gi--sm-1of4 pane__title">
-                <?php echo $page->title() ?>
-            </h3>
-            <div class="gi gi--full gi gi--sm-3of4">
+    <section class="gi--full gi--center">
+
+        <article class="g">
+            <div class="gi gi--full gi--sm-1of3">
+                <h3 itemprop="name">
+                    <?php echo $page->title() ?>
+                </h3>
+            </div>
+            <div class="gi gi--full gi--sm-2of3">
                 <?php echo kirbytext($page->text()) ?>
             </div>
         </article>
