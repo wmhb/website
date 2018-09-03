2fb6b2eea125997da59cce312fef9ac64a54b731
Dennis Gieseler <dennisausbremen@users.noreply.github.com>
2018-09-03 15:00:33 +0200

    Update API Plugin

--- site/plugins/api/api.php
+++ site/plugins/api/api.php
@@ -6,7 +6,7 @@ kirby()->routes([
     'pattern' => "{$prefix}/events",
     'action' => function() {
       // Get the projects pages
-      $projects = page('events')->children()->flip()->visible()->offset(0)->limit(1);
+      $projects = page('events')->children()->flip()->offset(0)->limit(2)->flip();
       $data = [];
       // Transform for API delivery
       foreach ($projects as $project) {
